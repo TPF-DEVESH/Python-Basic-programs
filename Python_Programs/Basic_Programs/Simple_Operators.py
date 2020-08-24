2f733ab0194751a89817ab9154f1751f1dad6d12
DEVESH KUMAR VERMA <69028322+TPF-DEVESH@users.noreply.github.com>
2020-08-24 20:23:39 +0530

    Create Simple_Operators.py

---
+++ Python_Programs/Basic_Programs/Simple_Operators.py
@@ -0,0 +1,11 @@
+#Simple operators can be executed on numbers and strings:
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "World -Python"
+helloworld = hello + " " + world
+print(helloworld)
